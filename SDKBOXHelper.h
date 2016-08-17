c89ac9814c04922b062ea11344cf1535022739c7
tokineco <tokinoneco@gmail.com>
2016-08-17 13:53:15 +0900

    SDKBOX:GoogleAnalytics復活

--- SDKBOXHelper.h
+++ SDKBOXHelper.h
@@ -14,7 +14,7 @@
 #include "cocos2d.h"
 
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WIN32)
-//#include "PluginGoogleAnalytics/PluginGoogleAnalytics.h"
+#include "PluginGoogleAnalytics/PluginGoogleAnalytics.h"
 #include "PluginIAP/PluginIAP.h"
 #endif
 
@@ -22,12 +22,12 @@ class SDKBOXHelper {
 
 public:
 
-    // // GoogleAnalyticsの初期化処理
-    // static void initAnalytics();
-    // // GoogleAnalyticsにScreen送信
-    // static void sendAnalyticsScreen(std::string screenName);
-    // // GoogleAnalyticsにEvent送信(スクリーン名指定)
-    // static void sendAnalyticsEvent(std::string screenName, std::string eventCategory, std::string eventAction, std::string eventLabel = "", int value = 0);
+     // GoogleAnalyticsの初期化処理
+     static void initAnalytics();
+     // GoogleAnalyticsにScreen送信
+     static void sendAnalyticsScreen(std::string screenName);
+     // GoogleAnalyticsにEvent送信(スクリーン名指定)
+     static void sendAnalyticsEvent(std::string screenName, std::string eventCategory, std::string eventAction, std::string eventLabel = "", int value = 0);
     
     // iAPの初期化処理
     static void initIAP();

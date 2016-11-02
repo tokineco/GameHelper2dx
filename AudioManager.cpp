32f0b337449411af91f3871d419fba1e65fbc57d
Shin Hirota <hirota@tomeapp.jp>
2016-11-02 09:55:50 +0900

    修正の箇所が間違っていたのを訂正
    １つまえのコミットの修正を、
    
     _bgmFileName = baseName;
    
    の後に移動しました。

--- AudioManager.cpp
+++ AudioManager.cpp
@@ -430,12 +430,12 @@ int AudioManager::playBgm(const std::string baseName, float fadeTime, bool loop,
             });
         }
     }
-
+    
+    _bgmFileName = baseName;
+	
     if (_bgmLoopList.count(_bgmFileName) > 0) {
         _bgmLoopList[_bgmFileName].isLoopInterval = false;
     }
-    
-    _bgmFileName = baseName;
     
     return _bgmId;
 }
